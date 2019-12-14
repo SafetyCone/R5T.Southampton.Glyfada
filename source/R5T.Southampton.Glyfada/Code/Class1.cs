2f61b9ea0dbe82706860c360a712410c777aa254
MinexAutomation <minex.automation@gmail.com>
2019-12-14 14:27:48 -0800

    Implemented a git-based source-control operator.

--- source/R5T.Southampton.Glyfada/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Southampton.Glyfada
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Southampton.Glyfada/Code/Services/Implementations/GitSourceControlOperator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using R5T.Glyfada;
+
+
+namespace R5T.Southampton.Glyfada
+{
+    public class GitSourceControlOperator : ISourceControlOperator
+    {
+        private IGitOperator GitOperator { get; }
+
+
+        public GitSourceControlOperator(IGitOperator gitOperator)
+        {
+            this.GitOperator = gitOperator;
+        }
+
+        public void Checkout(string repositoryUrl, string localDirectoryPath)
+        {
+            this.GitOperator.Clone(repositoryUrl, localDirectoryPath);
+        }
+    }
+}
