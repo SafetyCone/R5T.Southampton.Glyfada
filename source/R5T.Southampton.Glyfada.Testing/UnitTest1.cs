c26b17cbb280493e5bb00770f3c02094ae5b59af
MinexAutomation <minex.automation@gmail.com>
2019-12-14 14:21:55 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Southampton.Glyfada.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Southampton.Glyfada.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
